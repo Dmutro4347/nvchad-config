1e45aa5b3f45d78b63616f0406cd1c7f20386190
Dmutro4347 <dmutrobui@gmail.com>
2025-09-30 20:57:28 +0300

    mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -6,5 +6,24 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- map({ "n", "i", "v" }, "<C-q>", "<cmd> w <cr>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Split windows
+map("n", "|", ":vsplit<CR>", opts)
+map("n", "-", ":split<CR>", opts)
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize windows
+map("n", "<C-Up>", ":resize +2<CR>", opts)
+map("n", "<C-Down>", ":resize -2<CR>", opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Clear search highlight
+map({ "n", "v" }, ",<space>", ":nohlsearch<CR>", { noremap = true })
